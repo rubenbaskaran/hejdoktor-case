3e34f58a7a0dcf514985c9c6c11075d61af1180f
Ruben Baskaran <ruba@mmmi.sdu.dk>
2022-06-29 10:21:19 +0200

    added parent container for UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "teal",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <label
+        style={{
+          display: "flex",
+          width: "50%",
+          height: "50%",
+          backgroundColor: "lightblue",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Hello world
+      </label>
+    </div>
+  );
 }
 
 export default App;
