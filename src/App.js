eb743cc427697ec19d858994a13a64c401338c49
Ruben Baskaran <ruba@mmmi.sdu.dk>
2022-06-29 12:11:47 +0200

    added back/next buttons for changing question

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React from "react";
 import QuestionComponent from "./components/QuestionComponent";
 
 function App() {
+  const [questionNumber, setQuestionNumber] = React.useState(0);
+  const [listOfQuestions, setListOfQuestions] = React.useState([
+    { question: "What is your firstname?", type: "text" },
+    { question: "What is your lastname?", type: "text" },
+  ]);
+
+  function ChangeQuestion(direction) {
+    if (direction === "back") {
+      if (questionNumber !== 0) {
+        setQuestionNumber(questionNumber - 1);
+      }
+    } else if (direction === "next") {
+      if (questionNumber !== listOfQuestions.length) {
+        setQuestionNumber(questionNumber + 1);
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    console.log("Length: " + listOfQuestions.length);
+    console.log(questionNumber);
+  }, [questionNumber]);
+
   return (
     <div
       style={{
@@ -22,7 +46,7 @@ function App() {
           alignItems: "center",
         }}
       >
-        <QuestionComponent />
+        <QuestionComponent ChangeQuestion={ChangeQuestion} />
       </div>
     </div>
   );
--- src/components/QuestionComponent.js
+++ src/components/QuestionComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionComponent() {
+function QuestionComponent(props) {
   const [input, setInput] = React.useState("");
 
   return (
@@ -10,20 +10,91 @@ function QuestionComponent() {
         flexDirection: "column",
         width: "50%",
         height: "50%",
-        backgroundColor: "pink",
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <label>What is your name?</label>
-      <input type="text" onChange={(e) => setInput(e.target.value)}></input>
-      <button
-        onClick={() =>
-          console.log(`You've entered: ${input}. Going to next question!`)
-        }
+      <label
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "33%",
+          backgroundColor: "yellow",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
       >
-        Next question
-      </button>
+        What is your name?
+      </label>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "34%",
+          backgroundColor: "green",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input
+          style={{
+            display: "flex",
+            width: "90%",
+            height: "70%",
+          }}
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          height: "33%",
+          backgroundColor: "lightgreen",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignSelf: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "50%",
+            height: "100%",
+          }}
+        >
+          <button
+            style={{
+              width: "80%",
+              height: "80%",
+            }}
+            onClick={() => props.ChangeQuestion("back")}
+          >
+            Back
+          </button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignSelf: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "50%",
+            height: "100%",
+          }}
+        >
+          <button
+            style={{
+              width: "80%",
+              height: "80%",
+            }}
+            onClick={() => props.ChangeQuestion("next")}
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
